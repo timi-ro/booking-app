f1adcb7d3056294eaa440021ff09c3db7d8ad16c
alirezasaharkhiz <alireza.saharkhiz@outlook.com>
2025-11-30 19:08:47 +0330

    add todo

---
+++ app/Http/Controllers/HealthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseHelper;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\UserLoginRequest;
+use App\Http\Requests\Auth\UserRegisterRequest;
+use App\Services\AuthService;
+use Symfony\Component\HttpFoundation\Response;
+
+class HealthController extends Controller
+{
+    public function __construct(
+    ) {
+    }
+
+    public function healthCheck()
+    {
+        //TODO:
+        // check db (redis & mysql)
+        // check storage (ping & driver, [upload , download, delete {speed}])
+        // sys info (cpu, memory, ping with detail )
+        // email driver check (sen & driver info)
+        // cache driver detail
+    }
+}
