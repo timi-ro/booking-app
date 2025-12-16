5482c63cf7e72a86f9bc31ff1d631c59ada26ad2
Fatima Rostami <frostami.1988@gmail.com>
2025-12-16 22:27:39 +0300

    Handle all errors gracefully using our response template and add caceh mechanism for only one service(offering service)

--- app/Exceptions/OfferingDay/OfferingDayNotFoundException.php
+++ app/Exceptions/OfferingDay/OfferingDayNotFoundException.php
@@ -2,10 +2,21 @@
 
 namespace App\Exceptions\OfferingDay;
 
+use App\Helpers\ResponseHelper;
 use Exception;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class OfferingDayNotFoundException extends Exception
 {
-    protected $message = 'Offering day not found';
-    protected $code = 404;
+    public function __construct(protected $message = "Offering day not found", protected int $httpStatusCode = Response::HTTP_NOT_FOUND)
+    {
+        Log::channel('sentry')->error($this->message);
+        parent::__construct($message);
+    }
+
+    public function render()
+    {
+        return ResponseHelper::generateException($this, $this->httpStatusCode);
+    }
 }
--- app/Exceptions/OfferingTimeSlot/OfferingTimeSlotNotFoundException.php
+++ app/Exceptions/OfferingTimeSlot/OfferingTimeSlotNotFoundException.php
@@ -2,10 +2,21 @@
 
 namespace App\Exceptions\OfferingTimeSlot;
 
+use App\Helpers\ResponseHelper;
 use Exception;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class OfferingTimeSlotNotFoundException extends Exception
 {
-    protected $message = 'Time slot not found';
-    protected $code = 404;
+    public function __construct(protected $message = "Time slot not found", protected int $httpStatusCode = Response::HTTP_NOT_FOUND)
+    {
+        Log::channel('sentry')->error($this->message);
+        parent::__construct($message);
+    }
+
+    public function render()
+    {
+        return ResponseHelper::generateException($this, $this->httpStatusCode);
+    }
 }
